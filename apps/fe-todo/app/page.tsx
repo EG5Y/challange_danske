720a0e8686b490c46431796810416dbc96079e9b
night <>
2023-09-16 13:40:50 +0300

    refactor!: add nx, nextjs, typescript, eslint and tailwindcss

---
+++ apps/fe-todo/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <div>
+      <h1 className="text-green-500 text-lg font-bold">TEST gfgfgggggggggggggggg</h1>
+    </div>
+  );
+}
