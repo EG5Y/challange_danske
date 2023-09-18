a7e5ba7af6cf861d957a3f2882f12cec1a9a9dba
night <>
2023-09-18 07:20:33 +0300

    feature: todo items

--- apps/fe-todo/app/page.tsx
+++ apps/fe-todo/app/page.tsx
@@ -1,17 +1,22 @@
 
+function ListItem({ text }: {text: string}) {
+  return   <li className=" flex gap-4 p-4 bg-mid-one font-medium rounded-2xl">
+    <input type="checkbox"/>
+    <input type="text" value={text} className="bg-transparent"/>
+  </li>
+}
+
 export default async function Index() {
   return (
-    <main className="">
-      <header className="text-center">
-        <h1 className="text-[5rem]">Danske TODO</h1>
+    <main className="mx-auto w-4/5 flex flex-col gap-4">
+      <header className="pt-10">
+        <h1 className="text-[2rem]">Danske TODO</h1>
       </header>
-      <section className="mx-auto w-4/5 border-2 border-blue-500 p-5">
-        <ul className="border-2 border-red-500 p-5">
-          <li className=" p-4 bg-mid-one font-medium rounded-2xl">
-                <input type="checkbox"/>
-            <input type="text" value="Margherita Pizza"/>
-          </li>
-          <li className=" bg-high-one">wow</li>
+      <hr/>
+      <section className="   ">
+        <ul className=" flex flex-col gap-4 ">
+          <ListItem text="Margherita Pizza"/>
+          <ListItem text="Margherita Pizza"/>
         </ul>
       </section>
     </main>
