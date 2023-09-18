a4db31ca1d96223d91e4a89cf7c4ffe04bf5eb8c
night <>
2023-09-18 14:47:30 +0300

    refactor!: add bun workspaces

--- apps/fe-todo/tailwind.config.js
+++ apps/fe-todo/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./apps/fe-todo/index.html",
-    "./apps/fe-todo/src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
