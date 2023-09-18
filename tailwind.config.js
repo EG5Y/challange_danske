7b32ca8e54fab227cbb8ff55925887ab620aa7c9
night <>
2023-09-18 14:24:58 +0300

    refactor!: tailwindcss re-added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./apps/fe-todo/index.html",
+    "./apps/fe-todo/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
